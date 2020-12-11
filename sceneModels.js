7e067d319b7af6a51212801b4e8e54bff9ce161b
andre <almeida.a@code.ua.pt>
2020-12-11 20:31:39 +0000

    Added template files for modeling of 3D chess board and queen pieces

---
+++ sceneModels.js
@@ -0,0 +1,154 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  For instantiating the scene models.
+//
+//  J. Madeira - November 2018
+//
+//  Adapted by André Almeida - December 2020
+//
+//////////////////////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------------------------
+//
+//  Constructors
+//
+
+
+function emptyModelFeatures() {
+
+	// EMPTY MODEL
+
+	this.vertices = [];
+
+	this.normals = [];
+
+	// Transformation parameters
+
+	// Displacement vector
+	
+	this.tx = 0.0;
+	
+	this.ty = 0.0;
+	
+	this.tz = 0.0;	
+	
+	// Rotation angles	
+	
+	this.rotAngleXX = 0.0;
+	
+	this.rotAngleYY = 0.0;
+	
+	this.rotAngleZZ = 0.0;	
+
+	// Scaling factors
+	
+	this.sx = 1.0;
+	
+	this.sy = 1.0;
+	
+	this.sz = 1.0;		
+	
+	// Animation controls
+	
+	this.rotXXOn = true;
+	
+	this.rotYYOn = true;
+	
+	this.rotZZOn = true;
+	
+	this.rotXXSpeed = 1.0;
+	
+	this.rotYYSpeed = 1.0;
+	
+	this.rotZZSpeed = 1.0;
+	
+	this.rotXXDir = 1;
+	
+	this.rotYYDir = 1;
+	
+	this.rotZZDir = 1;
+	
+	// Material features
+	
+	this.kAmbi = [ 0.2, 0.2, 0.2 ];
+	
+	this.kDiff = [ 0.7, 0.7, 0.7 ];
+
+	this.kSpec = [ 0.7, 0.7, 0.7 ];
+
+	this.nPhong = 100;
+}
+
+function singleTriangleModel( ) {
+	
+	var triangle = new emptyModelFeatures();
+	
+	// Default model has just ONE TRIANGLE
+
+	triangle.vertices = [
+
+		// FRONTAL TRIANGLE
+		 
+		-0.5, -0.5,  0.5,
+		 
+		 0.5, -0.5,  0.5,
+		 
+		 0.5,  0.5,  0.5,
+	];
+
+	triangle.normals = [
+
+		// FRONTAL TRIANGLE
+		 
+		 0.0,  0.0,  1.0,
+		 
+		 0.0,  0.0,  1.0,
+		 
+		 0.0,  0.0,  1.0,
+	];
+
+	return triangle;
+}
+
+function boardModel(N) {
+
+	// TODO
+
+}
+
+function simpleTetrahedronModel( ) {
+	
+	var tetra = new emptyModelFeatures();
+	
+	tetra.vertices = [
+
+		-1.000000,  0.000000, -0.707000, 
+         0.000000,  1.000000,  0.707000, 
+         1.000000,  0.000000, -0.707000, 
+         1.000000,  0.000000, -0.707000, 
+         0.000000,  1.000000,  0.707000, 
+         0.000000, -1.000000,  0.707000, 
+        -1.000000,  0.000000, -0.707000, 
+         0.000000, -1.000000,  0.707000, 
+         0.000000,  1.000000,  0.707000, 
+        -1.000000,  0.000000, -0.707000, 
+         1.000000,  0.000000, -0.707000, 
+         0.000000, -1.000000,  0.707000,
+	];
+
+	computeVertexNormals( tetra.vertices, tetra.normals );
+
+	return tetra;
+}
+
+
+function tetrahedronModel( subdivisionDepth = 0 ) {
+	
+	var tetra = new simpleTetrahedronModel();
+	
+	midPointRefinement( tetra.vertices, subdivisionDepth );
+	
+	computeVertexNormals( tetra.vertices, tetra.normals );
+	
+	return tetra;
+}
